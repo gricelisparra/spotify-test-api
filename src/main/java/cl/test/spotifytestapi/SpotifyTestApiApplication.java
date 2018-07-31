410a83ac18de893e6e2507852d9b8661b0bca336
Gricelis Parra <gricelisparra@gmail.com>
2018-07-30 21:52:13 -0400

    Subida de esqueleto inicial con Spring Boot

---
+++ src/main/java/cl/test/spotifytestapi/SpotifyTestApiApplication.java
@@ -0,0 +1,12 @@
+package cl.test.spotifytestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyTestApiApplication.class, args);
+	}
+}
