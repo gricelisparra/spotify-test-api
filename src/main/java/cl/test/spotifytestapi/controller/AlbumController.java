ccd70415630112e87f8705ab29aecd5ed8519bb0
Gricelis Parra <gricelisparra@gmail.com>
2018-08-02 00:34:20 -0400

    Cambio tipo de retorno del metodo de busqueda de albums

--- src/main/java/cl/test/spotifytestapi/controller/AlbumController.java
+++ src/main/java/cl/test/spotifytestapi/controller/AlbumController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import cl.test.spotifytestapi.domain.AlbumPageWrapper;
@@ -31,24 +33,31 @@ public class AlbumController {
     @ResponseBody
     @RequestMapping(value = "/search", method = RequestMethod.GET)
     @CrossOrigin(origins = "*")
-    public AlbumPageWrapper searchAlbum(@RequestParam String albumName, @RequestParam(required=false) String artistName) {
+    public ResponseEntity<AlbumPageWrapper> searchAlbum(@RequestParam String albumName, @RequestParam(required=false) String artistName) {
     	
-    	HttpHeaders headers = new HttpHeaders();
-    	headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-    	headers.setContentType(MediaType.APPLICATION_JSON);
-    	headers.set("Authorization", "Bearer " + accessToken);
+    	ResponseEntity<AlbumPageWrapper> response = null;
     	
-    	HttpEntity<String> entity = new HttpEntity<String>(headers);
-    	 
-    	RestTemplate restTemplate = new RestTemplate();
-    	ResponseEntity<AlbumPageWrapper> response = restTemplate.exchange(
-			baseURL + "/search?type=album&q=album:" + albumName + (artistName != null ? " artist:" + artistName : ""),
-			HttpMethod.GET,
-			entity,
-			AlbumPageWrapper.class
-		);
+    	try {
+	    	HttpHeaders headers = new HttpHeaders();
+	    	headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+	    	headers.setContentType(MediaType.APPLICATION_JSON);
+	    	headers.set("Authorization", "Bearer " + accessToken);
+	    	
+	    	HttpEntity<String> entity = new HttpEntity<String>(headers);
+	    	 
+	    	RestTemplate restTemplate = new RestTemplate();
+	    	response = restTemplate.exchange(
+				baseURL + "/search?type=album&q=album:" + albumName + (artistName != null ? " artist:" + artistName : ""),
+				HttpMethod.GET,
+				entity,
+				AlbumPageWrapper.class
+			);
+    	} catch (HttpClientErrorException e) {
+    		e.printStackTrace(); // TODO: Cambiar a log
+    		return new ResponseEntity<AlbumPageWrapper>(e.getStatusCode());
+    	}
 		
-        return response.getBody();
+    	return new ResponseEntity<AlbumPageWrapper>(response.getBody(), HttpStatus.OK);
     }
 
 }
